8db183942d4ffc7bc1296d37379e3dc662cdd0a3
Jingcheng Su <jingches@uci.edu>
2020-04-08 13:57:32 -0700

    added a simple functionality: display first 20 movies combined JS, HTML and Java servlet

--- src/main/java/MovieListServlet.java
+++ src/main/java/MovieListServlet.java
@@ -1,17 +1,72 @@
+package main.java;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+
+@WebServlet(name = "MovieListServlet", urlPatterns = "/api/movielist")
 public class MovieListServlet extends HttpServlet{
 
-    protected void doGet( HttpServletRequest request,
-                          HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html");
+    @Resource(name = "jdbc/movies")
+    private DataSource dataSource;
+    protected void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
         PrintWriter out = response.getWriter();
-        out.println("testing testing");
+        try {
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+
+            // Declare our statement
+            Statement statement = dbcon.createStatement();
+
+            String query = "select title, year, director from movies limit 20";//need to be changed for proj1
+
+            ResultSet rs = statement.executeQuery(query);
+
+            JsonArray jsonArray = new JsonArray();
+
+            while (rs.next()){ //need to be changed for proj1
+                String movie_title = rs.getString("title");
+                String movie_year = rs.getString("year");
+                String movie_director = rs.getString("director");
+
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_title", movie_title);
+                jsonObject.addProperty("movie_year", movie_year);
+                jsonObject.addProperty("movie_director", movie_director);
+
+                jsonArray.add(jsonObject);
+            }
+            // write JSON string to output
+            out.write(jsonArray.toString());
+            // set response status to 200 (OK)
+            response.setStatus(200);
+
+            rs.close();
+            statement.close();
+            dbcon.close();
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // set reponse status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+        out.close();
     }
 }
---
+++ web/index.js
@@ -0,0 +1,30 @@
+function handleMovieListResult(resultData) {
+    console.log("handleMovieListResult: populating movieList table from resultData");
+
+    let movieListTableBodyElement = jQuery("#movieList_table_body");
+
+    for (let i = 0; i < Math.min(20, resultData.length); i++) {
+
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML += "<th>" + resultData[i]["movie_title"] + "</th>";    // display star_name for the link text
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        movieListTableBodyElement.append(rowHTML);
+    }
+}
+
+
+
+
+// Makes the HTTP GET request and registers on success callback function handleStarResult
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/movielist", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleMovieListResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
