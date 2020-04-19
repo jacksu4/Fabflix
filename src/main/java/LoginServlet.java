e0c8c9a39dc98b64184e8d7444418c3d2741cf4d
Jingcheng Su <jingches@uci.edu>
2020-04-18 22:32:12 -0700

    implemented login feature for proj2

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,77 @@
+package main.java;
+
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.lang.reflect.Field;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
+public class LoginServlet extends HttpServlet {
+    @Resource(name = "jdbc/moviedb")
+    private DataSource dataSource;
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        /* This example only allows username/password to be test/test
+        /  in the real project, you should talk to the database to verify username/password
+        */
+        PrintWriter out = response.getWriter();
+        try {
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+
+            // Declare our statement
+
+            String query = "select customers.email as Username, customers.password as Password from customers \n" +
+                    "where customers.email = ?";
+
+            PreparedStatement statement = dbcon.prepareStatement(query);
+
+            statement.setString(1, username);
+
+            ResultSet rs = statement.executeQuery();
+
+            JsonObject responseJsonObject = new JsonObject();
+
+            if (!rs.next()) {
+                responseJsonObject.addProperty("status", "fail");
+                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+            } else {
+                if (!rs.getString("Password").equals(password)) {
+
+                    responseJsonObject.addProperty("status", "fail");
+                    responseJsonObject.addProperty("message", "incorrect password");
+                } else {
+                    request.getSession().setAttribute("user", new User(username));
+
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
+                }
+            }
+
+            response.getWriter().write(responseJsonObject.toString());
+
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // set reponse status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+    }
+}
+
